09367fcb1ff245a51d55fb8064ffac3a58c34715
Gayratbek <118713014+gayrat6ek@users.noreply.github.com>
2022-12-01 15:46:56 +0500

    Update otpcode.py

--- modelapp/otpcode.py
+++ modelapp/otpcode.py
@@ -2,11 +2,6 @@ import random
 
 
 def otp_generator():
-    number_list = [x for x in range(5)]
-    code_list=[]
-    for number in range(5):
-        number = random.choice(number_list)
-        code_list.append(number)
-        code = "".join(str(item) for item in code_list)
+    code = random.randint(1000,9999)
     
     return code
